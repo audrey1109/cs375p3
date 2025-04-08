8e640ec28778e1a3e4e3bb345a5245b453b5bfc3
Audrey Palmer <audreypuke@doxxing-machine.local>
2025-04-08 12:22:12 -0400

    adding desmods

--- edit_distance.py
+++ edit_distance.py
@@ -15,3 +15,12 @@ def bottom_up_iterative(string1, string2) :
     '''
     bottom up : solve parts to combine for complete solution
     '''
+
+def top_down_recursive(string1, string2) :
+    '''
+    top down : divide and conquer the problem
+    '''
+    if len(string1) == 0 :
+        return len(string2)
+    if len(string2) == 0 :
+        return len(string1)
