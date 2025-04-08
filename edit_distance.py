4e21419e5cf9b33edde807fd0a52da6bacda6514
Audrey Palmer <audreypuke@doxxing-machine.local>
2025-04-08 12:14:37 -0400

    adding definitions

--- edit_distance.py
+++ edit_distance.py
@@ -7,7 +7,11 @@ edit_distance.py
 '''
 
 def top_down_recursive(sting1, string2) :
-    ''''''
+    '''
+    top down : divide and conquer the problem
+    '''
 
 def bottom_up_iterative(string1, string2) :
-    ''''''
+    '''
+    bottom up : solve parts to combine for complete solution
+    '''
